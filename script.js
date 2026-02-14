4b35b4a6a34579d205b6ff39477040ea4ec7f1bc
Lucas <lucasrian.dev@gmail.com>
2026-02-14 17:32:47 -0300

    Começando js

---
+++ script.js
@@ -0,0 +1,15 @@
+const inputBusca = document.querySelector("#input-search");
+const btnBusca = document.querySelector("#btn-search");
+
+const imgAvatar = document.querySelector("#img-avatar");
+const txtNome = document.querySelector("#txt-name");
+const txtLogin = document.querySelector("#txt-login");
+const txtBio = document.querySelector("#txt-bio");
+const txtSeguidores = document.querySelector("#txt-followers");
+const txtRepo = document.querySelector("#txt-repos");
+
+const btnFavoritar = document.querySelector("#btn-fav");
+
+async function buscarApi() {}
+
+btnBusca.addEventListener("click", buscarApi);
