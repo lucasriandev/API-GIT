56cfd90354b9b00bcbb60ac7117aca417fc5dffc
Lucas <lucasrian.dev@gmail.com>
2026-02-16 19:23:47 -0300

    api funcionando

--- script.js
+++ script.js
@@ -10,6 +10,35 @@ const txtRepo = document.querySelector("#txt-repos");
 
 const btnFavoritar = document.querySelector("#btn-fav");
 
-async function buscarApi() {}
+let usuarioAtual = JSON.parse(localStorage.getItem("Favoritos")) || [];
+
+async function buscarApi() {
+  const usuario = inputBusca.value;
+  if (usuario === "") return;
+
+  const url = `https://api.github.com/users/${usuario}`;
+
+  try {
+    const resposta = await fetch(url);
+    if (!resposta.ok) {
+      throw new Error("Erro na primeira fase!");
+    }
+
+    const dados = await resposta.json();
+    console.log(dados);
+
+    imgAvatar.src = dados.avatar_url;
+    txtNome.innerText = dados.name;
+    txtLogin.innerText = dados.login;
+    txtBio.innerText = dados.bio;
+    txtSeguidores.innerText = dados.followers;
+    txtRepo.innerText = dados.public_repos;
+
+    usuarioAtual = dados;
+  } catch (error) {
+    console.log(error);
+    alert("Erro");
+  }
+}
 
 btnBusca.addEventListener("click", buscarApi);
